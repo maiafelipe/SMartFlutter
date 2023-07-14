c2c3bef54570828d064057352a902397f67e67e7
fmaia85 <felipe.maia@ifce.edu.br>
2023-07-14 11:17:23 -0300

    Atualização e remoção de compras.

--- lib/models/dao/compra_dao.dart
+++ lib/models/dao/compra_dao.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:smart/models/compra.dart';
 import 'package:smart/services/database.dart';
 import 'package:sqflite/sqflite.dart';
@@ -26,8 +27,33 @@ class CompraDAO {
         maps[i]["descricao"],
         maps[i]["local"],
         id: maps[i]["id"],
+        status: maps[i]["status"] == describeEnum(CompraStatus.disable)
+            ? CompraStatus.disable
+            : CompraStatus.active,
       );
     });
+  }
+
+  static Future<Compra?> selectCompra(int id) async {
+    DBProvider dbp = DBProvider.getInstance();
+    Database db = await dbp.database;
+    final List<Map<String, dynamic>> maps = await db.query(
+      "compra",
+      where: "id=?",
+      whereArgs: [id],
+      limit: 1,
+    );
+    if (maps.isNotEmpty) {
+      return Compra(
+        maps.first["descricao"],
+        maps.first["local"],
+        id: maps.first["id"],
+        status: maps.first["status"] == describeEnum(CompraStatus.disable)
+            ? CompraStatus.disable
+            : CompraStatus.active,
+      );
+    }
+    return null;
   }
 
   static Future<void> deleteCompra(int id) async {
@@ -38,5 +64,18 @@ class CompraDAO {
       where: 'id = ?',
       whereArgs: [id],
     );
+  }
+
+  static Future<int> updateCompra(Compra compra) async {
+    DBProvider dbp = DBProvider.getInstance();
+    Database db = await dbp.database;
+    int id = await db.update(
+      'compra',
+      compra.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+      where: 'id = ?',
+      whereArgs: [compra.id],
+    );
+    return id;
   }
 }
